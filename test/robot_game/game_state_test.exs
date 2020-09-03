0f4602a13070eedb0ebdc03caac6cab906626b62
Daniel Wachtel <daniel.wachtel@allori.edu.au>
2020-09-03 13:24:30 +1000

    Combine place/1 and report/0 test

--- test/robot_game/game_state_test.exs
+++ test/robot_game/game_state_test.exs
@@ -7,11 +7,9 @@ defmodule RobotGame.GameStateTest do
     assert GameState.board == %{x: 5, y: 5}
   end
 
-  test "game initialises with no placement" do
-    assert GameState.report == %{}
-  end
-
   test "place/1 receives a map and updates state" do
+    assert GameState.report == %{}
+
     new_placement = %{x: 0, y: 2, direction: "SOUTH"}
     GameState.place(new_placement)
 
