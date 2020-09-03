7df2b224380d3b325ceefe020cfaa83fa940edcc
Daniel Wachtel <daniel.wachtel@allori.edu.au>
2020-09-03 22:04:43 +1000

    Enable async: true on tests

--- test/robot_game/game_state_test.exs
+++ test/robot_game/game_state_test.exs
@@ -1,5 +1,5 @@
 defmodule RobotGame.GameStateTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case, async: true
 
   alias RobotGame.GameState
 
--- test/robot_game/toy_robot_test.exs
+++ test/robot_game/toy_robot_test.exs
@@ -1,5 +1,5 @@
 defmodule RobotGame.ToyRobotTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case, async: true
 
   alias RobotGame.{GameState, ToyRobot}
 
